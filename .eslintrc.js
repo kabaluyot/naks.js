5b9e6e9f6492f8be665dbdc7bdd68c9ec36e6e9d
Karl Anthony Baluyot <sovietkamarov@gmail.com>
2020-05-02 00:24:53 +0800

    add eslint and prettier rules

---
+++ .eslintrc.js
@@ -0,0 +1,41 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    browser: true
+  },
+  plugins: ['prettier'],
+  globals: {
+    $nuxt: true
+  },
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  extends: [
+    'plugin:vue/recommended',
+    'eslint:recommended',
+    'prettier/vue',
+    'plugin:prettier/recommended'
+  ],
+  rules: {
+    'vue/component-name-in-template-casing': ['error', 'PascalCase'],
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+  },
+  overrides: [
+    {
+      // disable the rule for some config files
+      files: ['nuxt.config.js'],
+      rules: {
+        'no-unused-vars': 'off'
+      }
+    },
+    {
+      // disable the rule for test files
+      files: ['test/*.js'],
+      rules: {
+        'no-undef': 'off'
+      }
+    }
+  ]
+}
